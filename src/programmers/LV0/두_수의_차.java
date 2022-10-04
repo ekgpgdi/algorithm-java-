0020a4c884c9e2d81dbad97fe85df6a27b8909fb
ekgpgdi <dlekgp0423@naver.com>
2022-10-05 00:38:52 +0900

    feat: [2022-10-04] 두 수의 차

---
+++ src/programmers/LV0/두_수의_차.java
@@ -0,0 +1,15 @@
+package programmers.LV0;
+
+public class 두_수의_차 {
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 - num2;
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3) == -1);
+    }
+}
