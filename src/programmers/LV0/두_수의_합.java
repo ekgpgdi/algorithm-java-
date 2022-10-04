19a0a1b629ec16ed86edf26b6f8c34d97e476fc9
ekgpgdi <dlekgp0423@naver.com>
2022-10-04 17:22:51 +0900

    feat: [2022-10-04] 두 수의 합

---
+++ src/programmers/LV0/두_수의_합.java
@@ -0,0 +1,17 @@
+package programmers.LV0;
+
+import java.util.Arrays;
+
+public class 두_수의_합 {
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 + num2;
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3) == 5);
+    }
+}
