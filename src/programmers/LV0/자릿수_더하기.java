7a796967f5decad1c80e379a4e9efdae10825d23
dahye <>
2022-10-05 12:19:51 +0900

    feat: [2022-10-05] 자릿수 더하기

---
+++ src/programmers/LV0/자릿수_더하기.java
@@ -0,0 +1,16 @@
+package programmers.LV0;
+
+public class 자릿수_더하기 {
+    public static int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1234) == 10);
+    }
+}
