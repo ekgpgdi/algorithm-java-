cd579c57d5849a76d34abf936bd65edb4547dc31
ekgpgdi <dlekgp0423@naver.com>
2022-10-13 00:28:47 +0900

    feat: [2022-10-11] 합성수 찾기

---
+++ src/programmers/LV0/합성수_찾기.java
@@ -0,0 +1,16 @@
+package programmers.LV0;
+
+public class 합성수_찾기 {
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 4; i <= n; i++) {
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10) == 5);
+    }
+}
