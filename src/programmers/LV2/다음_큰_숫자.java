e672b0979b770e4ec6ccaf382eabd2bacc81f15a
ekgpgdi <dahae80912@gmail.com>
2024-08-23 14:08:56 +0900

    feat: [2024-08-23] 다음 큰 숫자

---
+++ src/programmers/LV2/다음_큰_숫자.java
@@ -0,0 +1,12 @@
+package programmers.LV2;
+
+public class 다음_큰_숫자 {
+  public static int solution(int n) {
+    int answer = 0;
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(78) == 83);
+  }
+}
