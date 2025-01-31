13388335c759b95fe73c72205721e5db6dae3729
ekgpgdi <dlekgp0423@naver.com>
2025-01-31 15:08:01 +0900

    feat: [2025-01-30] N개의 최소 공배수

--- src/programmers/LV2/N개의_최소공배수.java
+++ src/programmers/LV2/N개의_최소공배수.java
@@ -1,48 +1,27 @@
 package programmers.LV2;
 
+import java.util.Arrays;
+
 public class N개의_최소공배수 {
     public static int solution(int[] arr) {
-        int answer = 1;
-        int divide = 2;
-        boolean check = true;
+        Arrays.sort(arr);
 
-        while(true) {
-            for(divide = 2; divide <= 3; divide ++) {
-                check = true;
-                for(int i =0; i < arr.length; i++) {
-                    if(arr[i] % divide != 0) {
-                        check = false;
-                        break;
-                    }
-                }
-                if(divide == 2 && check) {
-                    break;
-                }
-            }
-            
-            if(divide > 3) {
-                divide = 3;
-            }
-
-            if(check) {
-                for(int i =0; i< arr.length; i++) {
-                    arr[i] /= divide;
-                }
-                answer *= divide;
-
-            }
-
-            if(!check && divide == 3) {
-                for(int i =0; i< arr.length; i++) {
-                    answer *= arr[i];
-                }
-                break;
-            }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int num = gcd(arr[i + 1], arr[i]);
+            // 최소공배수(LCM) 계산: (a * b) / GCD(a, b)
+            arr[i + 1] = (arr[i] * arr[i + 1]) / num;
         }
-        return answer;
+
+        return arr[arr.length - 1];
+    }
+
+    // 최소 공약수를 구하는 유클리드 호제법
+    private static int gcd(int a, int b) {
+        if (a % b == 0) return b;
+        return gcd(b, a % b);
     }
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{100,100}));
+        System.out.println(solution(new int[]{2, 6, 8, 14}));
     }
 }
