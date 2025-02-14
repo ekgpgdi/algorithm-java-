bf9d6a60a262f7c40a748abfe694c9f6d3519193
ekgpgdi <dlekgp0423@naver.com>
2025-02-14 15:51:17 +0900

    feat: [2025-02-14] 예상 대진표

--- src/programmers/LV2/예상_대진표.java
+++ src/programmers/LV2/예상_대진표.java
@@ -4,14 +4,19 @@ public class 예상_대진표 {
     public static int solution(int n, int a, int b) {
         int answer = 0;
 
-        while (a != b) {
-            if (a % 2 == 1) a++;
-            if (b % 2 == 1) b++;
-
-            a = a / 2 + 1;
-            b = b / 2 + 1;
-            answer ++;
+        if (a > b) {
+            int tmp = b;
+            b = a;
+            a = tmp;
         }
+
+        do {
+            a = a / 2 + a % 2 ;
+            b = b / 2 + b % 2 ;
+            answer++;
+
+        } while (a != b);
+
         return answer;
     }
 
